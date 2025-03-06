9e959e7d5872d902990003a63198d6492416b591
Maxim <mudrak.maksym@chnu.edu.ua>
2025-03-06 21:39:15 +0200

    Lab1 Done

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,28 @@
-
-
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        humanIMB humanIMB = new humanIMB(80,1.52);
-        System.out.println(humanIMB.Result());
+        HumanIMB human = new HumanIMB(80, 1.52);
+        System.out.println(human.getResult());
     }
 }
-class humanIMB {
-    public double W; //Weight Human
-    public double H; // Height Human
-    private static double imb;
-    public humanIMB(double w, double h) {
-        W = w;
-        H = h;
-        imb = W / (H * H);
+
+class HumanIMB {
+    private double weight;
+    private double height;
+
+    public HumanIMB(double weight, double height) {
+        this.weight = weight;
+        this.height = height;
     }
-    public double takeW() {
-        return W;
+
+    public double calculateIMB() {
+        return weight / (height * height);
     }
-    public void putW(double w) {
-        W = w;
-        imb = W / (H * H);
-    }
-    public double takeH() {
-        return H;
-    }
-    public void putH(double h) {
-        H = h;
-        imb = W / (H * H);
-    }
-    public static double takeImt() {
-        return imb;
-    }
-    public static String Result() {
-        String  string = null;
-        if (imb >=18.5 & imb <25) {
-            string ="Norm";
-        }
-        if (imb >=25 & imb <30) {
-            string ="Warning! ";
-        }
-        if (imb >=30) {
-            string ="Fat";
-        }
-        if (imb <18.5) {
-            string ="Deficit";
-        }
-        return string;
+
+    public String getResult() {
+        double imb = calculateIMB();
+        if (imb < 18.5) return "Deficit";
+        if (imb < 25) return "Norm";
+        if (imb < 30) return "Warning!";
+        return "Fat";
     }
 }
